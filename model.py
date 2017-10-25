76460793cfbebd9d66a43a51d437ac5890d2fba0
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2017-10-25 13:09:24 +0200

    Implement functions: check value, modify name and description

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ class ToDoList():
 class ToDoItem():
 
     def __init__(self, name, description, is_done=False):
-
+        
         NAME_LENGTH = 20
         DESTRIPCTION_LENGTH = 150
 
@@ -23,3 +23,31 @@ class ToDoItem():
             self.description = description
         self.is_done = is_done
 
+    def modify_name(self, name):
+        NAME_LENGTH = 20
+        if ToDoItem.check_value(name, NAME_LENGTH):
+            self.name = name
+        return name
+
+    def modify_description(self, description):
+        DESTRIPCTION_LENGTH = 150
+        if ToDoItem.check_value(description, DESTRIPCTION_LENGTH):
+            self.description = description
+        return description
+
+    @classmethod    
+    def check_value(cls, value, length):
+        if len(value) <= length:
+            return True
+        else:
+            raise ValueError("Too long value")     
+
+    def __str__(self):
+        return "Name: " + self.name + "\nDescription: " + self.description + "\nStatus: " + str(self.is_done)
+
+item = ToDoItem("Zrobić zakupy", "Lista zakupów: pomidor, chleb")
+print(item)
+#print(ToDoItem.modify_name("Pranie", 20))
+#print(ToDoItem.check_value("Zrobić zakupy", 20))
+#print(ToDoItem.modify_name(item, "Pranie"))
+#print(ToDoItem.modify_description(item, "Blalalalalalallalala"))
