a724c4eb43d0b5e06a19c98899d2995b306aa5d1
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2017-10-25 21:48:28 +0200

    Added startView function

---
+++ view.py
@@ -0,0 +1,11 @@
+def startView():
+    print("1.Add item")
+    print("2.Modify item")
+    print("3.Delete item")
+    print("4.Mark item as done")
+    print("5.Display items list")
+    print("6.Display specific todo item's details")
+    print("7.Exit")
+    print("  Choose action:")
+
+startView()
